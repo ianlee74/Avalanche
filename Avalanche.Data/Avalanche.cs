eeaa60729e892cc9c53b10347933dc3b9222a9cd
Ian Lee <ian@houseoflees.net>
2013-02-20 00:38:26 -0600

    Initial commit.

---
+++ Avalanche.Data/Avalanche.cs
@@ -0,0 +1,9 @@
+﻿namespace Avalanche.Data
+{
+    public class Avalanche
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int ParentAvalancheId { get; set; }
+    }
+}
