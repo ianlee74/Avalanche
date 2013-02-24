9f9cf40ca8604a4c59ee4db0ddb6ae7a1d4ca6d6
Ian Lee <ian@houseoflees.net>
2013-02-24 12:56:39 -0600

    Added Sketchflow project.

---
+++ Sketchflow/Longsor/Longsor/LongsorScreens/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Longsor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Longsor")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("305bd8fd-f630-4ac3-87f3-e36cf5924670")]
---
+++ Sketchflow/Longsor/Longsor/LongsorScreens/Stabilization_Electronics.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace LongsorScreens
+{
+	public partial class Stabilization_Electronics : UserControl
+	{
+		public Stabilization_Electronics()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
